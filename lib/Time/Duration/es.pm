ff2bd6bb8c60831a85ca3c0a0082cdca0b086298
Biafra <biafra@cpan.org>
2017-03-23 15:05:33 +0000

    Minor distribution corrections

--- lib/Time/Duration/es.pm
+++ lib/Time/Duration/es.pm
@@ -2,7 +2,7 @@ package Time::Duration::es;
 use strict;
 use warnings;
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 use base qw(Exporter);
 our @EXPORT = qw( later later_exact earlier earlier_exact
